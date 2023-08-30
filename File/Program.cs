a22b82309533caa68d0ecbb1954eb1e3182440e4
Koros_zl65@top-academy.ru <Koros_zl65@top-academy.ru>
2023-08-31 01:24:33 +0500

    Done

--- File/Program.cs
+++ File/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿//#define WRITE_TO_FILE
+#define READ_FRON_FILE
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,19 +13,42 @@ namespace File
     {
         static void Main(string[] args)
         {
+#if WRITE_TO_FILE
+
             string currentDirectory = Directory.GetCurrentDirectory();
             string filename = "File.txt";
             Console.WriteLine(currentDirectory);
             Console.WriteLine(Directory.GetCurrentDirectory());
-            StreamWriter sw = new StreamWriter("File.txt");
-            sw.WriteLine("Hello");
+            //StreamWriter sw = new StreamWriter(filename); 
+            StreamWriter sw = new StreamWriter(filename,true); // добавление в конец ФАЙЛА
+            sw.WriteLine("Hello BOB");
             sw.Close();
             Console.WriteLine(currentDirectory);
             Console.WriteLine(filename);
             string cmd = currentDirectory + "\\" + filename;
             Console.WriteLine(cmd);
-            System.Diagnostics.Process.Start("notepad",cmd);
+            System.Diagnostics.Process.Start("notepad", cmd); 
+
+#endif
+
+
+#if READ_FRON_FILE
             
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string filename = "File.txt";
+            Console.WriteLine(currentDirectory);
+            StreamReader sr = new StreamReader(filename); 
+            for (int i=1; !sr.EndOfStream; i++)
+            {
+                string buffer = sr.ReadLine();
+                Console.WriteLine(i + " " + buffer);
+            }
+
+
+#endif
+
+
+
         }
     }
 }
