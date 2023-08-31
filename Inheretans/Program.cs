2426be3f213c5d4e5c9ad4df1d778749c189d4aa
Koros_zl65@top-academy.ru <Koros_zl65@top-academy.ru>
2023-08-31 18:55:15 +0500

    Academy

--- Inheretans/Program.cs
+++ Inheretans/Program.cs
@@ -1,16 +1,19 @@
 ﻿// Наследование 
+//#define WRITE_TO_FILE
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Text.RegularExpressions;
+using System.Runtime.Remoting.Messaging;
 
-namespace Inheretans
+namespace Academy
 {
     internal class Program
     {
-        const string delimiter = "\n======================================\n";
+        const string delimiter = "\n=================================================\n";
         static void Main(string[] args)
         {
             #region Inheritance
@@ -26,15 +29,16 @@ namespace Inheretans
             //Graduate hank = new Graduate("Puchkin", "Alex", 23, "Criminalist", "WW20", 70, 99, " how much is the fish");
             //Console.WriteLine(hank); 
             #endregion
-            
-            
+
+#if WRITE_TO_FILE
+
             // добавление 
             Student tommi = new Student("Stalone", "Silver", 33, "Theft", "Vice", 80, 80);
             Specialist billy = new Specialist("Balboa", "Rooky", 24, "Box", "Vice", 80, 80, "Happy", 12);
             Teacher diaz = new Teacher("Roma", "Pupkin", 45, "Matimatic", 12);
-            
+
             // полиморфизм 
-            Human [] group = new Human[]
+            Human[] group = new Human[]
             {
                 new Student("Pinkman", "Jessie", 25, "Chemestry", "SS_200", 80, 90),
                 new Teacher("Caesar", "Gay", 50, "Sportman", 20),
@@ -44,10 +48,10 @@ namespace Inheretans
             string currentDirectory = Directory.GetCurrentDirectory();
             string filename = "group.txt";
             StreamWriter streamWriter = new StreamWriter(filename);
-            for (int i =0; i<group.Length;i++)
+            for (int i = 0; i < group.Length; i++)
             {
-                Console.WriteLine(group[i]);
-                streamWriter.WriteLine(group[i]);
+                Console.WriteLine(group[i]+ ";");
+                streamWriter.WriteLine(group[i]+";");
                 //group[i].Print();
                 Console.WriteLine(delimiter);
             }
@@ -55,6 +59,48 @@ namespace Inheretans
             string cmd = currentDirectory + "\\" + filename;
             System.Diagnostics.Process.Start("group.txt", cmd);
             streamWriter.Close();
+
+#endif
+            Load("group.txt");
         }
+        static Human[] Load(string filename)
+        {
+            Human[] group = null;
+            List<Human> l_group =new List<Human> { };
+            StreamReader stream = new StreamReader(filename);
+            while(!stream.EndOfStream)
+            {
+                string buffer = stream.ReadLine();
+                // разбить строку можно методом String.Split
+                string[] values = buffer.Split(new char[] {':',',',';'});
+                //Console.WriteLine( buffer);
+                //foreach(string i in values)Console.Write( i+"\t");
+                //Console.WriteLine();
+                //Console.WriteLine( delimiter);
+                l_group.Add(HumanFactory(values[0]));
+                //l_group.Last().GetType();
+                l_group.Last();
+            }
+            stream.Close();
+            return group;
+        }
+        static Human HumanFactory(string type)
+        {
+            Human obj = null;
+            if (type == typeof(Academy.Student).ToString()) obj = new Student("", "", 0, "", "", 0, 0);
+            if (type == typeof(Academy.Teacher).ToString()) obj = new Teacher("", "", 0, "", 0);
+            if (type == typeof(Academy.Graduate).ToString()) obj = new Graduate("", "", 0, "", "", 0, 0,"");
+            if (type == typeof(Academy.Specialist).ToString()) obj = new Specialist("", "", 0, "", "", 0, 0,"",0);
+            return obj;
+        }
+        static Human InitHuman(Human obj, string[] values)
+        {
+            if (obj.GetType() == typeof(Academy.Student))
+            {
+
+            }
+            return obj;
+        }
+       
     }
 }
